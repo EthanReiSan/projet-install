40fd05ed072916dbaa03f9b97dc7d603af62c05a
Maxime-Herrera <114060993+Maxime-Herrera@users.noreply.github.com>
2022-09-22 14:13:24 +0200

    issi ont parles phransser

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -5,5 +5,6 @@ int main()
     std::cout << "Hello, World!" << endl;
     std::cout << "Hello, World 2!" << endl;
     std::cout << "Hello, Patata con patas!" << endl;
+    std::cout << "Hello, issi ont parles le phransser" << endl;
     return 0;
 }
