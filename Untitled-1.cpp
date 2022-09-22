1fb6e9b335333872123154083db9c885e040bf63
Solrac118 <114059527+Solrac118@users.noreply.github.com>
2022-09-22 14:23:41 +0200

    le code de la patata

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -5,6 +5,5 @@ int main()
     std::cout << "Hello, World!" << endl;
     std::cout << "Hello, World 2!" << endl;
     std::cout << "Hello, Patata con patas!" << endl;
-    std::cout << "Hello, issi ont parles le phransser" << endl;
     return 0;
 }
